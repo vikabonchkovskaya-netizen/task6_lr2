9fcb947121660aab7d14fb81b174c641ed0e66fd
prostto_vika <vikabonchkovskaya@gmail.com>
2025-09-27 23:15:43 +0300

    var3

--- task_6/task6_lr2.cpp
+++ task_6/task6_lr2.cpp
@@ -1,26 +1,19 @@
-#include <iostream>
+﻿#include <iostream>
 #include <cmath>
-using namespace std;
 
 int main()
 {
-    double x, b, D, A, S;
-    cout << "Enter x, D: " << endl;
-    cin >> x >> D;
-    b = x + D;
-    if (b == 0)
-    {
-        cout << "division by zero" << endl;
+    double x, p, C, D, n, K, B, Q;
+    std::cout << "Enter x, p, n, K, D" << std::endl;
+    std::cin >> x >> p >> n >> K >> D;
+    B = cos(x);
+    C = p - n;
+    if (K * D == 0) {
+        std::cout << "division by zero" << std::endl;
         return 1;
     }
-    A = D*(x/b);
-    double del = pow(D, 3) + (A + D - b);
-    if (del == 0) 
-    {
-        cout << "division by zero" << endl;
-        return 1;
-    }
-    S = (A * A + b * cos(x)) / del;
-   cout <<S;
-    return 0; 
+
+    Q = pow(B, 2) / (K * D) + B * pow(C, 3);
+    std::cout << Q;
+    return 0;
 }
