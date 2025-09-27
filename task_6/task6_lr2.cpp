6e77a8b4f8399bc1bd8a025e801ee34678856a4b
Vika <vikabonchkovskaya@gmail.com>
2025-09-28 00:01:17 +0300

    var10

--- task_6/task6_lr2.cpp
+++ task_6/task6_lr2.cpp
@@ -3,16 +3,16 @@
 
 int main()
 {
-    double A,x,z,B,p,Y,C,K,D;
-    std::cout << "Enter x,z,p,K,C,D" << std::endl;
-    std::cin >> x >> z >> p >> C >> K >> D ;
-    if (C*D == 0) {
+    double A,x,k,B,z,Y,C,D;
+    std::cout << "Enter x,z,k,C,D" << std::endl;
+    std::cin >> x >> z >> k >> C >> D ;
+    if (A == 0) {
         std::cout << "division by zero" << std::endl;
         return 1;
     }
-    A = sin(x)-z;
-    B = fabs(p-x);
-    Y = pow(A+B,2) - K/C*D ;
+    A = log(x)-k;
+    B = sqrt(z);
+    Y = pow(D,2)+ pow(C,2)/0.75*A + B ;
     std::cout << Y;
     return 0;
 }
